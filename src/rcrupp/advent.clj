aeec20fd88b51f3f4bbfe95f262bdd08b2e84bd3
rcrupp <rcrupp@gmail.com>
2022-12-02 08:31:11 -0500

    day 2

--- src/rcrupp/advent.clj
+++ src/rcrupp/advent.clj
@@ -19,11 +19,37 @@
 (defn aoc1b []
   (reduce + (take 3 (reverse (sort (sum-up (str/split-lines (slurp "input.txt"))))))))
 
+(def score_choice {"X" 1 "Y" 2 "Z" 3})
+
+(def score_win {"AX" 3 "AY" 6 "AZ" 0
+                "BX" 0 "BY" 3 "BZ" 6
+                "CX" 6 "CY" 0 "CZ" 3})
+
+(defn score [x]
+  (+ (score_choice (second x)) (score_win (str/join x))))
+
+
+(defn aoc2a []
+  (reduce + (map score (map #(str/split % #" ") (str/split-lines (slurp "input2.txt"))))))
+
+(def score2b_win {"X" 0 "Y" 3 "Z" 6})
+
+(def score2b_choice {"AX" 3 "AY" 1 "AZ" 2 
+                     "BX" 1 "BY" 2 "BZ" 3 
+                     "CX" 2 "CY" 3 "CZ" 1})
+
+(defn score2b [x]
+  (+ (score2b_win (second x)) (score2b_choice (str/join x))))
+
+
+(defn aoc2b []
+  (reduce + (map score2b (map #(str/split % #" ") (str/split-lines (slurp "input2.txt"))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (aoc1))
+  (aoc1a))
 
 (comment
   
@@ -31,7 +57,11 @@
   ;; => (100 200 nil 400)
   
   (sum-up ["100" "200" "" "300" "400" "" "500" "600"])
+  
+  (score ["B" "Z"])
 
   (aoc1a)
   (aoc1b)
+  (aoc2a)
+  (aoc2b)
   )
